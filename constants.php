c067cecfccb10cd2cf57b94c1ae8334b5e701e49
dpoulter <dalepoulter@gmail.com>
2016-11-11 21:03:52 +0000

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,38 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * Global constants.
+     */
+
+    // your database's name
+    define("DATABASE", "database1");
+
+    // your database's password
+    define("PASSWORD", "pass");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "username");
+	
+	//smtp host name
+	define("SMTP_HOST","smtp.email.com");
+	
+	//smtp user name
+	define("SMTP_USERNAME","smtpusername");
+	
+	//smtp password
+	define("SMTP_PASSWORD","smtppassword");
+	
+	//smtp port
+	define("SMTP_PORT","587");
+	
+	//site_url
+	define ("SITE_URL","http://mysite.com")
+	
+	
+
+?>
