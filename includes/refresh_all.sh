ebae66e29d8a14b5a1a9d3d0414299ceb79021f8
dpoulter <dalepoulter@gmail.com>
2023-07-18 18:03:09 +0100

    refresh_all

--- includes/refresh_all.sh
+++ includes/refresh_all.sh
@@ -1,6 +1,6 @@
-#php /var/www/includes/scrape_fin_stmts.php
-php /var/www/includes/get_share_prices.php
-php /var/www/includes/scrape_key_statistics.php
-php /var/www/includes/get_api_stats.php
-php /var/www/includes/get_statistics.php
-php /var/www/includes/performance_function.php
+python3 /var/www/shares/python/fetch_stock_info.py
+python3 /var/www/shares/python/get_api_stats.py
+php /var/www/shares/includes/get_share_prices.php
+#php /var/www/shares/includes/get_momentum_statistics.php
+php /var/www/shares/includes/get_statistics.php
+php /var/www/shares/includes/performance_function.php
